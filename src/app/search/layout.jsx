f51e99b0d30a05ae5048035a4f4465329cce5d49
Aboubacar Traore <aboubatrao04@gmail.com>
2024-12-04 13:16:57 +0100

    updated

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const metadata = {
+    title:"Search-Results - Atmosphere"
+}
+
+export default function layout({ children }) {
+  return <div>{children}</div>;
+}
